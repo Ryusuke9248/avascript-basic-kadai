578630eb777fb4371d837aa8506f8e3ad9e3e200
Ryusuke9248 <146909431+Ryusuke9248@users.noreply.github.com>
2023-12-14 06:01:08 +0900

    Update event.js

--- kadai_20/event.js
+++ kadai_20/event.js
@@ -1,8 +1,8 @@
-var btn = document.getElementById("btn");
+const btn = document.getElementById("btn");
 
 btn. addEventListener("click", function() {
 
-  var textElement = document.getElementById("text");
+  const textElement = document.getElementById("text");
 
   textElement.textContent="ボタンをクリックしました";
 });
