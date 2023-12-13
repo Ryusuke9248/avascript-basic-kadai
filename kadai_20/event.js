b7d3763a84c744c1c291f0bcd1d5a458e3df0f7b
Ryusuke9248 <146909431+Ryusuke9248@users.noreply.github.com>
2023-12-14 05:58:32 +0900

    Add files via upload

---
+++ kadai_20/event.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById("btn");
+
+btn. addEventListener("click", function() {
+
+  var textElement = document.getElementById("text");
+
+  textElement.textContent="ボタンをクリックしました";
+});
+
